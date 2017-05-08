df7c2cdc9008399d74fb0c060ef466e1c8f1f4f2
Steven Yap <stevenyap@futureworkz.com>
2017-05-08 14:38:09 +0800

    Make react-hot-loader a dev dependency

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,101 @@
+/**
+ * Responsibility: Webpack config for development env
+ */
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin')
+const path = require('path')
+const rootDir = path.join(__dirname, '..')
+
+// *** Configuration Settings ***
+
+// Environment variables
+const env = {
+  __DEV__: true,
+  'process.env': {
+    FIREBASE_API_KEY: '""',
+    FIREBASE_AUTH_DOMAIN: '""',
+    FIREBASE_DATABASE_URL: '""'
+  }
+}
+
+// Base URL for single page routing
+const baseUrl = 'http://localhost:8000/'
+
+// *** End Configuration Settings ***
+
+// Webpack config
+const config = {
+  context: path.join(rootDir, '/src'),
+  entry: [
+    // activate HMR for React
+    'react-hot-loader/patch',
+
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
+    'webpack-dev-server/client?http://localhost:8000',
+
+    // bundle the client for hot reloading
+    // only- means to only hot reload for successful updates
+    'webpack/hot/only-dev-server',
+
+    // the entry point of our app
+    './index.js'
+  ],
+  devServer: {
+    hot: true,
+    historyApiFallback: true,
+    contentBase: './public',
+    port: 8000
+  },
+
+  output: {
+    filename: 'bundle.js',
+    path: path.join(rootDir, '/public')
+  },
+
+  resolve: {
+    modules: ['node_modules', 'src']
+  },
+
+  module: {
+    loaders: [
+      // Process JS files with babel
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      // JSON is not enabled by default in Webpack but both Node and Browserify
+      // allow it implicitly so we also enable it.
+      {
+        test: /\.json$/,
+        loader: 'json'
+      }
+    ]
+  },
+
+  plugins: [
+    // enable HMR globally
+    new webpack.HotModuleReplacementPlugin(),
+
+    // prints more readable module names in the browser console on HMR updates
+    new webpack.NamedModulesPlugin(),
+
+    // Makes some environment variables available to the JS code
+    new webpack.DefinePlugin(env),
+
+    // Generates an `index.html` file with the <script> injected.
+    new HtmlWebpackPlugin({
+      template: path.join(rootDir, 'config/index.ejs'),
+      baseUrl: baseUrl
+    }),
+
+    // Watcher doesn't work well if you mistype casing in a path so we use
+    // a plugin that prints an error when you attempt to do this.
+    // See https://github.com/facebookincubator/create-react-app/issues/240
+    new CaseSensitivePathsPlugin()
+  ]
+}
+
+module.exports = config
