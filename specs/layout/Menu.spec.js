9e7f0a06bacbb59731097e8ddd421f87d5e0f848
Steven Yap <stevenyap@futureworkz.com>
2017-05-09 12:13:47 +0800

    Fix Menu test cases

--- specs/layout/Menu.spec.js
+++ specs/layout/Menu.spec.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Menu } from 'layout/Menu'
 
 describe('Menu', () => {
-  it('renders correctly', () => {
+  it('renders correctly for public', () => {
     const tree = renderJSON(
       <BrowserRouter>
         <Menu />
@@ -13,6 +13,20 @@ describe('Menu', () => {
     expect(tree).toMatchSnapshot()
 
     const renderedText = toText(tree)
-    expect(renderedText).toContain('Home')
+    expect(renderedText).toContain('React Stack')
+  })
+
+  it('renders correctly for registered users', () => {
+    const admin = { uid: 'UID' }
+    const tree = renderJSON(
+      <BrowserRouter>
+        <Menu admin={admin} />
+      </BrowserRouter>
+    )
+    expect(tree).toMatchSnapshot()
+
+    const renderedText = toText(tree)
+    expect(renderedText).toContain('React Stack')
+    expect(renderedText).toContain('Logout')
   })
 })
