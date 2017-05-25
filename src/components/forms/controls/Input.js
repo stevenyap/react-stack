393d62ff6a7bd8762b255dd93a67d2b4e0c208a5
Steven Yap <stevenyap@futureworkz.com>
2017-05-25 16:16:40 +0800

    Add addon to Input

--- src/components/forms/controls/Input.js
+++ src/components/forms/controls/Input.js
@@ -2,17 +2,27 @@
  * @flow
  * Responsibility: We receive props from ReduxForm's Field and turn them into props for Bootstrap forms
  */
+import type { Children } from 'react'
+
 import React from 'react'
 import {
   FormGroup,
+  InputGroup,
   ControlLabel,
   FormControl,
   HelpBlock
 } from 'react-bootstrap/lib'
 
-type Props = { input: Object, meta: Object, label: string, type: ?string }
+type Props = {
+  input: Object,
+  meta: Object,
+  label: string,
+  type: ?string,
+  addon?: string | Children,
+  addonPost?: string | Children
+}
 const Input = (props: Props) => {
-  const { input, meta, label, type = 'text' } = props
+  const { input, meta, label, type = 'text', addon, addonPost } = props
   // pass onBlur to enable touched flag
   // pass onChange so the ReduxForm can work
   const { value, onChange, onBlur } = input
@@ -22,12 +32,16 @@ const Input = (props: Props) => {
   return (
     <FormGroup validationState={validationState}>
       <ControlLabel>{label}</ControlLabel>
-      <FormControl
-        type={type}
-        onBlur={onBlur}
-        onChange={onChange}
-        value={value}
-      />
+      <InputGroup>
+        {addon && <InputGroup.Addon>{addon}</InputGroup.Addon>}
+        <FormControl
+          type={type}
+          onBlur={onBlur}
+          onChange={onChange}
+          value={value}
+        />
+        {addonPost && <InputGroup.Addon>{addonPost}</InputGroup.Addon>}
+      </InputGroup>
       {touched && error && <HelpBlock>{error}</HelpBlock>}
       <FormControl.Feedback />
     </FormGroup>
